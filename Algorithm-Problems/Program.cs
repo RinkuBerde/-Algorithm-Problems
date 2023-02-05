4f82404ac6742314018b10d5ceb1302ffd3001a1
rinkuberde <rinkuberde@gmail.com>
2023-02-05 16:21:27 +0530

    [Rinku] - permutations of a String using iterative method and Recursion method.

--- Algorithm-Problems/Program.cs
+++ Algorithm-Problems/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Algorithm-Problems!!!");
+            Console.WriteLine("\t\tWelcome to Algorithm-Problems!!!");
+            Console.WriteLine("---------------------------------------------------------------------");
+            while (true)
+            {
+                Console.WriteLine("\n 1. Program to return all permutations of a string");
+                Console.Write("Enter your choice : ");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        PermutationsOfString permute = new PermutationsOfString();
+                        permute.Driver();
+                        break;
+                    default:
+                        Console.WriteLine("Please choose the correct option");
+                        break;
+                }
+            }
         }
     }
 }
