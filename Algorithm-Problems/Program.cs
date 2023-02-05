45509faec172c2a5cce53e378cd4e90ad2cbaccc
rinkuberde <rinkuberde@gmail.com>
2023-02-05 15:51:43 +0530

    [Rinku] - Welcome Message'

---
+++ Algorithm-Problems/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Algorithm_Problems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Algorithm-Problems!!!");
+        }
+    }
+}
