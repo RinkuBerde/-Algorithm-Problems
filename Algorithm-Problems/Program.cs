19c7ee9f3cd0465f0fcb1d0f3eddb3ea78304457
rinkuberde <rinkuberde@gmail.com>
2023-02-05 16:48:51 +0530

    [Rinku] -Added Code for Insertion Sort Algorithm

--- Algorithm-Problems/Program.cs
+++ Algorithm-Problems/Program.cs
@@ -10,6 +10,8 @@
             {
                 Console.WriteLine("\n 1. Program to return all permutations of a string");
                 Console.WriteLine(" 2. Binary Search the word from word list");
+                Console.WriteLine(" 3. Insertion sort");
+                Console.WriteLine(" 0. Exit");
                 Console.Write("Enter your choice : ");
                 int choice = Convert.ToInt32(Console.ReadLine());
                 switch (choice)
@@ -22,6 +24,12 @@
                         BinarySearchWord binarySearchWord = new BinarySearchWord();
                         binarySearchWord.DriverMethod();
                         break;
+                    case 3:
+                        InsertionSort insertionSort = new InsertionSort();
+                        insertionSort.Insertion();
+                        break;
+                    case 0:
+                        return;
                     default:
                         Console.WriteLine("Please choose the correct option");
                         break;
